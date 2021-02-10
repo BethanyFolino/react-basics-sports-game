5cb04005e18804ef4ff706882b364f14921f38f3
BETHANY FOLINO <bethany.folino@gmail.com>
2021-02-10 13:28:30 -0800

    Made objects for 2 teams and added logo pictures

--- reactApp.js
+++ reactApp.js
@@ -1,14 +1,22 @@
 // Deafault App component that all other compents are rendered through
-function App(props){
+function App(props) {
+  const teamOne = {
+    name: "Gallifrey Time Lords",
+    logo: "./time-lords-logo.jpg",
+  };
+  const teamTwo = {
+    name: "Planet Mondas Cybermen",
+    logo: "./cybermen-logo.png",
+  };
+
   return (
     <div>
       <h1>Welcome to the sports game starter</h1>
+      <Body team={teamOne} />
+      <Body team={teamTwo} />
     </div>
-  )
+  );
 }
 
 //Render the application
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
