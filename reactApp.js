9a323cf977f293bbb49b5a33a30d9118b67b1d71
BETHANY FOLINO <bethany.folino@gmail.com>
2021-02-10 22:29:49 -0800

    Made React fragment inside App to store team name and logo props, made Team class with name and logo and ability to render name and logo

--- reactApp.js
+++ reactApp.js
@@ -1,42 +1,103 @@
-// Deafault App component that all other compents are rendered through
-
-class HomeTeam extends React.Component {
-  constructor(props) {
-    super(props);
-    this.name = "Gallifrey Time Lords";
-    // this.logo = //why won't it make a logo and not just a string version of the path to the logo?
-  }
-
-  render() {
-    return (
-      <h1>
-        Team Name: {this.name} {this.logo}
-      </h1>
-    );
-  }
-}
-class AwayTeam extends React.Component {
-  constructor(props) {
-    super(props);
-    this.name = "Planet Mondas Cybermen";
-    // this.logo = something it actually recognizes as a logo NOT a string
-  }
-
-  render() {
-    return (
-      <h1>
-        Team Name: {this.name} {this.logo}
-      </h1>
-    );
-  }
-}
+// Default App component that all other components are rendered through
 function App(props) {
   return (
-    <div>
-      <HomeTeam />
-      <AwayTeam />
-    </div>
+    <React.Fragment>
+      <Game
+        name=""
+        homeTeam="Gallifrey Time Lords"
+        awayTeam="Planet Mondas Cybermen"
+        homeLogo="./time-lords-logo.jpg"
+        awayLogo="./cybermen-logo.png"
+      />
+    </React.Fragment>
   );
+}
+
+class Team extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shots: 0,
+      score: 0,
+
+      name: props.name,
+      logo: props.logo,
+    };
+  }
+
+  //Shots taken
+  shotsTaken = () => {
+    this.setState((...state) => ({
+      shots: this.state.shots + 1,
+    }));
+  };
+
+  //Score
+  score = () => {
+    let number = Math.floor(Math.random() * 2);
+    if (number == 0) {
+      this.setState((...state) => ({
+        score: this.state.score + 1,
+      }));
+    }
+  };
+
+  //Shot percentage
+  shotPercentage = () => {
+    this.setState((state) => {
+      return {
+        ...state,
+        percentage: Math.floor((state.score / state.shots) * 100),
+      };
+    });
+  };
+
+  //Event handler
+  onClick = (event) => {
+    this.shotsTaken();
+    this.score();
+    this.shotPercentage();
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.name}</h2>
+        <img src={this.props.logo} width="200" />
+        <br />
+        Shots: {this.state.shots}
+        <br />
+        Score: {this.state.score}
+        <br />
+        {this.state.shots ? (
+          <div>Shot Percentage: {this.state.percentage + "%"}</div>
+        ) : (
+          <div></div>
+        )}
+        <br />
+        <br />
+        <button onClick={this.onClick}>Shoot!</button>
+        <br />
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Welcome to {this.props.name}!</h1>
+        <Team name={this.props.homeTeam} logo={this.props.homeLogo} />
+        <Team name={this.props.awayTeam} logo={this.props.awayLogo} />
+      </React.Fragment>
+    );
+  }
 }
 
 //Render the application
