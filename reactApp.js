dac4c83aaee17d0533f3d9b2da93c0f0867f45b6
BETHANY FOLINO <bethany.folino@gmail.com>
2021-02-10 16:41:01 -0800

    Classes instead of objects. But logo pictures won't show up.

--- reactApp.js
+++ reactApp.js
@@ -1,19 +1,40 @@
 // Deafault App component that all other compents are rendered through
-function App(props) {
-  const teamOne = {
-    name: "Gallifrey Time Lords",
-    logo: "./time-lords-logo.jpg",
-  };
-  const teamTwo = {
-    name: "Planet Mondas Cybermen",
-    logo: "./cybermen-logo.png",
-  };
 
+class HomeTeam extends React.Component {
+  constructor(props) {
+    super(props);
+    this.name = "Gallifrey Time Lords";
+    // this.logo = //why won't it make a logo and not just a string version of the path to the logo?
+  }
+
+  render() {
+    return (
+      <h1>
+        Team Name: {this.name} {this.logo}
+      </h1>
+    );
+  }
+}
+class AwayTeam extends React.Component {
+  constructor(props) {
+    super(props);
+    this.name = "Planet Mondas Cybermen";
+    // this.logo = something it actually recognizes as a logo NOT a string
+  }
+
+  render() {
+    return (
+      <h1>
+        Team Name: {this.name} {this.logo}
+      </h1>
+    );
+  }
+}
+function App(props) {
   return (
     <div>
-      <h1>Welcome to the sports game starter</h1>
-      <Body team={teamOne} />
-      <Body team={teamTwo} />
+      <HomeTeam />
+      <AwayTeam />
     </div>
   );
 }
